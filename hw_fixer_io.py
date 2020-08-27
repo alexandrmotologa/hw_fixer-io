5050c62efa0f226b20d0e54b569012cc5c656af6
alexandrmotologa <al.motologa@gmail.com>
2020-08-27 07:19:48 +0300

    inceput

--- hw_fixer_io.py
+++ hw_fixer_io.py
@@ -27,9 +27,9 @@ else:
     file.close()
 
 
-def convert(money_mdl):
-    money_usd = money_mdl * usd / mdl
-    return money_usd
+def convert(your_money, your_val, our_val ):
+    exch = your_money * our_val / your_val
+    return round(exch, 3)
 
 
 if data['success'] is False:
@@ -39,4 +39,44 @@ else:
     mdl = data['rates']['MDL']
     usd = data['rates']['USD']
     rub = data['rates']['RUB']
-    print(convert(1_000_000))
+
+
+def disp():
+    a = " -- "
+    print(f"Valuta disponibila la data de {ymd}")
+    print(f"Referinta valutei la EUR dupa fixer.io")
+    print("\n")
+    print(f"EUR:  [{a:^12}]")
+    print(f"MDL:  [{round(mdl, 4):^12}] ")
+    print(f"USD:  [{round(usd * 1 / 1, 4):^12}]")
+    print(f"RUB:  [{round(rub, 4):^12}]")
+
+
+def menu():
+    try:
+        option = -1
+        while option != 0:
+            print("\n")
+            print("%%%%%%%%%% WELCOME TO EXCHANGE APP %%%%%%%%%%")
+            print("$$$$$ MDL $$$$$ EUR $$$$ USD $$$$$ RUB $$$$$")
+            print("> 1. EXCHANGE TERMINAL")
+            print("> 2. Valuta disponibila")
+            print("> 3. LATITUDE AND LONGITUDE OF CITY")
+            print("> 4. ORGANIZATION")
+            print("> 5. Internet service provider IP/DOMAIN")
+            print("> 0. Exit")
+            print("$$$$$ MDL $$$$$ EUR $$$$ USD $$$$$ RUB $$$$$")
+            print("CHOOSE OPTION > ")
+
+            option = int(input())
+            if option == 0:
+                exit()
+            if option == 1:
+                print(convert(eval(input("introduceti suma pentru schimb valutar: ")),
+                              eval(input("Introduceti valuta de care dispuneti: ").lower()),
+                              eval(input("Introduceti valuta in care va avea loc schimbul valutar: ").lower())))
+            if option == 2:
+                disp()
+    except ValueError:
+        menu()
+menu()
