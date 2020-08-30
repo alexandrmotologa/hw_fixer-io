b1a215920333151e289fabb5898e95a215d57491
alexandrmotologa <al.motologa@gmail.com>
2020-08-30 04:31:28 +0300

    try

--- HW_fixer_io.py
+++ HW_fixer_io.py
@@ -3,56 +3,34 @@ import json
 import datetime
 from os import path
 
-today = datetime.datetime.now()
-ymd = (str(today)).split(' ')[0]
+
+# YYMMDD
+
+def yymm(y = None):
+    if y is None:
+        today = datetime.datetime.now()
+        ymd = (str(today)).split(' ')[0]
+        return ymd
+    else:
+        ymd = datetime.datetime(int(y[0]), int(y[1]), int(y[2]))
+        ymd.strftime("%Y-%m-%d")
+        ymd = str(ymd.strftime("%Y-%m-%d"))
+        return ymd
+
+ymd = yymm()
+
+# YYMMDD END
 
 
-def year_constructor(y):
-    ymd = datetime.datetime(int(y[0]), int(y[1]), int(y[2]))
-    ymd.strftime("%Y-%m-%d")
-    ymd = str(ymd.strftime("%Y-%m-%d"))
-    return ymd
+# NEW FILE or READ OLD FILE
 
-
-def option_4(val):
-    start = datetime.datetime.strptime(year_constructor(str(input("Introduceti anul de la care incepem perioada afisarii (de ex. 2020 01 01): ")).split()), "%Y-%m-%d")
-
-    end = datetime.datetime.strptime(year_constructor(str(input("Introduceti anul cu care se va incheia perioada afisarii (de ex. 2020 01 03): ")).split()), "%Y-%m-%d")
-    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days+1)]
-
- #  global file_name este aici #######################@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-    for date in date_generated:
-        global file_name
-        ymd = date.strftime("%Y-%m-%d")
-        file_name = f'./rates--{ymd}.json'
-        # access data
-        key = '825c07d7bc2238ec3c43cbe5ac7aefef'
-        global endpoint
-        endpoint = 'http://data.fixer.io/api/' + ymd + '?access_key=' + key
-        fl_operation()
-        tr4 = transform_rates(data['rates'])
-        pr_tr4 = tr4['EUR']['to'][val]
-        print(f'{ymd} - {pr_tr4}')
-    return ymd, date_generated
-
-
- #  global file_este si aici, altfel nu lucreaza cum trebuie :DDD dar oricum nu e corect nici 2 global ?! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-def fl_key():
-    global file_name
+def new_file(ymd):
     file_name = f'./rates--{ymd}.json'
+
     # access data
     key = '825c07d7bc2238ec3c43cbe5ac7aefef'
-    global endpoint
     endpoint = 'http://data.fixer.io/api/' + ymd + '?access_key=' + key
-    return ymd
-
-
-fl_key()
-
-
-# check cache
-def fl_operation():
+    # check cache
     global data
     if path.exists(file_name):
         file = open(file_name, 'r')
@@ -65,10 +43,13 @@ def fl_operation():
         file = open(file_name, 'w')
         file.write(response.text)
         file.close()
+    if data['success'] is False:
+        print("CANNOT ACCESS DATA")
+    else:
+        tr = transform_rates(data['rates'])
+    return tr, data
 
-
-fl_operation()
-
+# TRANSFORM RATES
 
 def transform_rates(original):
     transform_rates = {}
@@ -78,31 +59,23 @@ def transform_rates(original):
             transform_rates[code1]['to'][code2] = original[code2] / original[code1]
     return transform_rates
 
+tr, data = new_file(yymm())
 
-tr = transform_rates(data['rates'])
 
+# NEW FILE + TRANSFORM RATES END
+
+
+# CONVERT / OPTION_1
 
 def convert(your_money, your_val, our_val):
-    # exch = your_money * asm
     exch = your_money * tr[your_val]['to'][our_val]
 
     return round(exch, 3)
 
 
-def option_3(val):
-    global ymd
-    y = str(input("Introduceti anul (de ex. 2020 01 01) : ")).split()
-    ymd = datetime.datetime(int(y[0]), int(y[1]), int(y[2]))
-    ymd.strftime("%Y-%m-%d")
-    ymd = str(ymd.strftime("%Y-%m-%d"))
-    fl_key()
-    fl_operation()
-    tr3 = transform_rates(data['rates'])
-    pr_tr3 = tr3['EUR']['to'][val]
-    print(pr_tr3)
+# DISPLAY / OPTION_2
 
-
-def disp():
+def disp(ymd):
     a = " -- "
     print(f"Valuta disponibila la data de {ymd}")
     print(f"Referinta valutei la EUR dupa fixer.io")
@@ -113,8 +86,42 @@ def disp():
     print(f"RUB:  [{round(tr['EUR']['to']['RUB'], 4):^12}] ")
 
 
+# OPTION_3
+
+def option_3(val):
+    y = str(input("Introduceti anul (de ex. 2020 01 01) : ")).split()
+    new_file(yymm(y))
+    tr3 = transform_rates(data['rates'])
+    pr_tr3 = tr3['EUR']['to'][val]
+    print(pr_tr3)
+
+
+# OPTION_3 END
+
+
+# OPTION_4
+
+def option_4(val):
+    start = datetime.datetime.strptime(yymm(str(input("Introduceti anul de la care incepem perioada afisarii (de ex. 2020 01 01): ")).split()), "%Y-%m-%d")
+
+    end = datetime.datetime.strptime(yymm(str(input("Introduceti anul cu care se va incheia perioada afisarii (de ex. 2020 01 03): ")).split()), "%Y-%m-%d")
+    date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days+1)]
+
+    for date in date_generated:
+        ymd = date.strftime("%Y-%m-%d")
+        new_file(ymd)
+        tr4 = transform_rates(data['rates'])
+        pr_tr4 = tr4['EUR']['to'][val]
+        print(f'{ymd} - {pr_tr4}')
+    return ymd, date_generated
+
+
+# OPTION_4 END
+
+
+# MENU
+
 def menu():
-    global ymd
     try:
         option = -1
         while option != 0:
@@ -134,18 +141,22 @@ def menu():
                 exit()
             if option == 1:
                 print(convert(float(input("introduceti suma pentru schimb valutar: ")),
-                              input("Introduceti valuta de care dispuneti: ").upper(),
-                              input("Introduceti valuta in care va avea loc schimbul valutar: ").upper()))
+                                str(input("Introduceti valuta de care dispuneti: ").upper()),
+                                str(input("Introduceti valuta in care va avea loc schimbul valutar: ").upper())))
             if option == 2:
-                disp()
+                disp(yymm())
             if option == 3:
                 option_3(input("Introduceti denumirea monedei interesate: ").upper())
-                today = datetime.datetime.now()
-                ymd = (str(today)).split(' ')[0]
             if option == 4:
                 option_4(input("Introduceti denumirea monedei interesate: ").upper())
     except ValueError:
+        print("ERROR TRY AGAIN")
         menu()
-
+    except KeyError:
+        print("ERROR TRY AGAIN")
+        menu()
+    except IndexError:
+        print("ERROR TRY AGAIN")
+        menu()
 
 menu()
--- lesson/test/new.py
+++
@@ -1,6 +0,0 @@
-import datetime
-y = str(input("y: ")).split()
-
-x = datetime.datetime(int(y[0]), int(y[1]), int(y[2]))
-
-print(x.strftime("%Y-%m-%d"))
